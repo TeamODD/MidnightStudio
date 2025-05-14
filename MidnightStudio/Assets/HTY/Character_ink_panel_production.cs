e3a2fd32d76f7f4b43fd0f5da7ca00f0be879541
WagaTonoka <hdh060209@gmail.com>
2025-05-15 05:43:23 +0900

    메인 약간 수정
    ㄹㅇ

--- MidnightStudio/Assets/HTY/Character_ink_panel_production.cs
+++ MidnightStudio/Assets/HTY/Character_ink_panel_production.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class Character_ink_panel_production : MonoBehaviour
 {
     public UI_Production ui_production;
+    public Obj_Production Obj;
     public RectTransform ink_panel_Rect;
 
     public void MoveRight_Ink()
     {
         ui_production.Move("Smooth", 0.2f, "x", ink_panel_Rect.anchoredPosition.x, -3.36f);
+        Obj.Move("Smooth", true, 0.2f, "x", Obj.transform.position.x, 1.5f);
     }
     public void MoveLeft_Ink()
     {
-        ui_production.Move("Smooth", 0.2f, "x", ink_panel_Rect.anchoredPosition.x, -7.12f);
+        ui_production.Move("Smooth", 0.2f, "x", ink_panel_Rect.anchoredPosition.x, -7.83f);
+        Obj.Move("Smooth", true, 0.2f, "x", Obj.transform.position.x, -1.5f);
     }
     public void GameStartInkMove_panel() {
         ui_production.Move("Smooth", 1f, "x", ink_panel_Rect.anchoredPosition.x, -6.22f);
